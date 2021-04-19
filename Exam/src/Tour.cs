71d5b255d41a04566f61eb07eac839ca868aca5a
stu-pksp117 <stu-pksp117@AMDVLA3511GW04>
2021-04-19 12:37:21 +0300

    created a domain object

---
+++ Exam/src/Tour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam.src
+{
+    class Tour
+    {
+        private string destination;
+        private int durationInHours;
+        private decimal price;
+
+        public Tour(string destination, int durationInHours, decimal price)
+        {
+            this.destination = destination;
+            this.durationInHours = durationInHours;
+            this.price = price;
+        }
+
+        public string Destination { get => destination; set => destination = value; }
+        public int DurationInHours { get => durationInHours; set => durationInHours = value; }
+        public decimal Price { get => price; set => price = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
--- Exam/src/TourSystem.cs
+++ Exam/src/TourSystem.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Exam.src;
+using System;
 
 namespace Exam
 {
-    class Program
+    class TourSystem
     {
         static void Main(string[] args)
         {
+            Tour tour = new Tour();
+           
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
